63cff647db3006ba2dd1605e58057293767a1916
Yauheni <curiosityopportunity@gmail.com>
2022-03-07 19:50:55 +0300

    array-list-ConcurrentModificationException

--- src/com/mikhalevich/concurrent/ConcurrentList.java
+++ src/com/mikhalevich/concurrent/ConcurrentList.java
@@ -1,4 +1,51 @@
 package com.mikhalevich.concurrent;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class ConcurrentList {
+    public static void main(String[] args) throws InterruptedException {
+        List<String> list = new ArrayList<String>();
+        list.add("Ivan");
+        list.add("Oleg");
+        list.add("Nikolai");
+        list.add("John");
+        list.add("Fedor");
+        System.out.println(list);
+
+        Runnable runnable1 = () -> {
+            Iterator<String> iterator = list.iterator();
+            while(iterator.hasNext()){
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                System.out.println(iterator.next());
+            }
+        };
+
+        Runnable runnable2 = () -> {
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                list.remove(4);
+                list.add("Elena");
+        };
+
+        Thread thread1 = new Thread(runnable1);
+        Thread thread2 = new Thread(runnable2);
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+        System.out.println(list);
+
+    }
 }
