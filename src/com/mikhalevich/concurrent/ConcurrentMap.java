f2d50bd66852137b0f83bf74e9c671a0d5dff274
Yauheni <curiosityopportunity@gmail.com>
2022-03-07 20:58:48 +0300

    concurrent-map-ConcurrentModificationException

--- src/com/mikhalevich/concurrent/ConcurrentMap.java
+++ src/com/mikhalevich/concurrent/ConcurrentMap.java
@@ -1,4 +1,52 @@
 package com.mikhalevich.concurrent;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class ConcurrentMap {
+    public static void main(String[] args) throws InterruptedException {
+        Map<Integer, String> map = new HashMap<Integer, String>();
+        map.put(1, "One");
+        map.put(2, "Two");
+        map.put(3, "Three");
+        map.put(4, "Four");
+        map.put(5, "Five");
+        System.out.println(map);
+
+        Runnable runnable1 = () -> {
+            Iterator<Integer> iterator = map.keySet().iterator();
+            while (iterator.hasNext()){
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Integer i = iterator.next();
+                System.out.println(i + ":" + map.get(i));
+            }
+        };
+
+        Runnable runnable2 = () -> {
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            map.put(6, "Elena");
+        };
+
+        Thread thread1 = new Thread(runnable1);
+        Thread thread2 = new Thread(runnable2);
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+        System.out.println(map);
+
+
+    }
 }
